58713c73d9f1437880b53cb6942e2479840af0c0
609529897 <609529897@qq.com>
2020-06-27 15:47:24 +0800

    添加分号

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-import './styles/index.scss'
-export { default as QuaStore} from './quaStore'
-export { default as Lazyimg } from './lazyimg'
-export { default as Placebo} from './placebo'
-export { default as Icon } from './icon'
-export { default as Button} from './button'
-export { default as Switch } from './switch'
+import './styles/index.scss';
+export { default as QuaStore} from './quaStore';
+export { default as Lazyimg } from './lazyimg';
+export { default as Placebo} from './placebo';
+export { default as Icon } from './icon';
+export { default as Button} from './button';
+export { default as Switch } from './switch';
